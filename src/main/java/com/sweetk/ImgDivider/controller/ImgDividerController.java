1c10a45af982a24bf828439e8562a8ef02852775
hsh0367 <hsh0367@naver.com>
2019-12-05 03:41:47 +0900

    ADD: select person controller

--- src/main/java/com/sweetk/ImgDivider/controller/ImgDividerController.java
+++ src/main/java/com/sweetk/ImgDivider/controller/ImgDividerController.java
@@ -2,12 +2,15 @@ package com.sweetk.ImgDivider.controller;
 
 import com.sweetk.ImgDivider.domain.ImgDividerDomain;
 import com.sweetk.ImgDivider.service.ImgDividerService;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -20,30 +23,31 @@ public class ImgDividerController {
     @Autowired
     ImgDividerService ImgDividerService;
 
-    // 전체 보기
     @GetMapping("/")
-    public ModelAndView list() throws Exception{
-        List<ImgDividerDomain> boardList = ImgDividerService.findAll();
-        ModelAndView nextPage = new ModelAndView("ImgDivider/test");
-        nextPage.addObject("boardList", boardList);
+    public ModelAndView readTest() throws Exception{
+        List<ImgDividerDomain> person_list = ImgDividerService.findPerson_nm();
+        ModelAndView nextPage = new ModelAndView("ImgDivider/ImgDivider");
+        nextPage.addObject("personlist", person_list);
         return nextPage;
     }
+    @GetMapping("/person/{personName}")
+    public ModelAndView selectPerson( @PathVariable String personName) throws Exception{
+        List<ImgDividerDomain> person_list = ImgDividerService.findPerson_nm();
+        List<ImgDividerDomain> img_list = ImgDividerService.findByPerson_nm(personName);
+        ModelAndView mv = new ModelAndView("/ImgDivider/ImgDivider");
+        System.out.println(personName);
+        mv.addObject("personlist", person_list);
+        mv.addObject("imglist", img_list);
+        return  mv;
+    }
 
-    // 글 상세보기
-//    @GetMapping("/board/{bno}")
-//    public ModelAndView readOne(@PathVariable("bno") int bno) throws Exception{
-//        ModelAndView popupPage = new ModelAndView("board/read");
-//        popupPage.addObject("board", Seivice.findByBno(bno));
-//        return popupPage;
-//    }
-
-    // TEST
-    @GetMapping("/test")
-    public ModelAndView readTest() throws Exception{
-        List<ImgDividerDomain> boardList = ImgDividerService.findAll();
-        ModelAndView nextPage = new ModelAndView("test");
-        nextPage.addObject("boardList", boardList);
-        return nextPage;
+    @RequestMapping(value = "/img/{img_no}")
+    @ResponseBody
+    public void update(@RequestParam int img_no,@RequestParam int is_use)throws Exception{
+        System.out.println("REQUST TEST update --- "+img_no);
+        ImgDividerDomain img = ImgDividerService.findByImg_no(img_no);
+        img.setIs_use(is_use);
+        ImgDividerService.updateImg_no(img);
     }
 
 }
